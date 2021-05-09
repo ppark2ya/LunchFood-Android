2962ece459fbb2bbd7b8f0f0882a161eccecb50e
ppark2ya <vigabhan1231@gmail.com>
2021-05-09 23:17:12 +0900

    1. gradle > databinding 추가

--- app/src/main/java/com/lunchfood/ui/main/view/HomeFragment.kt
+++ app/src/main/java/com/lunchfood/ui/main/view/HomeFragment.kt
@@ -79,6 +79,18 @@ class HomeFragment: BaseFragment() {
     }
 
     private fun setUserLocation() {
+        /**
+            // 중심점 변경
+            mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+            // 줌 레벨 변경
+            mapView.setZoomLevel(7, true);
+            // 중심점 변경 + 줌 레벨 변경
+            mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
+            // 줌 인
+            mapView.zoomIn(true);
+            // 줌 아웃
+            mapView.zoomOut(true);
+         */
         val userNowLocation = MapPoint.mapPointWithGeoCoord(userLat, userLon)
 
         MapPOIItem().let { userMarker ->
